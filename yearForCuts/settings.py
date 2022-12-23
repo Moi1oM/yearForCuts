9dd1cb7695cc0cb49cb078b5e4467b8631eb3a51
Kim Jiseong <35622664+Kim-Jiseong@users.noreply.github.com>
2022-12-23 14:53:41 +0900

    test

--- yearForCuts/settings.py
+++ yearForCuts/settings.py
@@ -69,7 +69,7 @@ AUTH_USER_MODEL = 'accounts.User'
 # AUTH_USER_MODEL = 'accounts.CustomUser' 
 # CORS 관련 추가
 CORS_ORIGIN_WHITELIST = ['http://127.0.0.1:3000'
-                         ,'http://localhost:3000','.cloudtype.app']
+                         ,'http://localhost:3000','cloudtype.app']
 CORS_ALLOW_CREDENTIALS = True
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': (
